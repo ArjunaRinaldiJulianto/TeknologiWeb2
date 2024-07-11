d03fd8fc46675de583226b3df8a36e5942c121d0
ArjunaRinaldiJulianto <arjuna565rinaldi@gmail.com>
2024-07-11 10:31:41 +0700

    Pertemuan 16

--- inertia-react/app/Http/Controllers/PostController.php
+++ inertia-react/app/Http/Controllers/PostController.php
@@ -21,4 +21,83 @@ class PostController extends Controller
             'posts' => $posts
         ]);
     }
+    
+    public function create()
+    {
+        return inertia ('Posts/Create');
+    }
+    
+    /**
+     * Summary of store
+     * @param Request $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function store (Request $request)
+    {
+        //set validation
+        $request->validate([
+            'title' => 'required', 
+            'content' => 'required',
+        ]);
+
+        //create post
+        Post::create([
+            'title' => $request->title,
+            'content' => $request->content
+        ]);
+        
+        //redirect
+        return redirect()->route('posts.index')->with('success', 'Data Berhasil Disimpan!');
+    }
+
+    /**
+     * Summary of edit
+     * @param Post $post
+     * @return \Inertia\Response|\Inertia\ResponseFactory
+     */
+    public function edit (Post $post)
+    {
+        return inertia('Posts/Edit', [
+            'post' => $post,
+        ]);
+    }
+    
+    /**
+     * Summary of update
+     * @param Request $request
+     * @param Post $post
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function update(Request $request, Post $post)
+    {
+        //set validation
+        $request->validate([
+            'title' => 'required',
+            'content' => 'required',
+        ]);
+
+        //update post
+        $post->update([
+            'title'=> $request->title,
+            'content' => $request->content
+        ]);
+
+        //redirect
+        return redirect()->route('posts.index')->with('success', 'Data Berhasil Diupdate!');
+    }
+
+    /**
+     * Summary of destroy
+     * @param Post $post
+     * @return \Illuminate\Http\RedirectResponse
+     * @throws \Exception
+     */
+    public function destroy(Post $post)
+    {
+        //delete post
+        $post->delete();
+
+        //redirect
+        return redirect()->route('posts.index')->with('success', 'Data Berhasil Dihapus!');
+    }
 }
